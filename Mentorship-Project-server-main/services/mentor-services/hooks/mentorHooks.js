dc19f8687b7941b0ec4fb476f90725aaf85ca120
mehulagarwal13 <mehulagarwal333@gmail.com>
2025-12-04 01:25:59 +0530

    new

--- Mentorship-Project-server-main/services/mentor-services/hooks/mentorHooks.js
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-export const hashPasswordHook = async function (next) {
-  if (!this.isModified('password')) return next(); // only hash if changed
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-};
